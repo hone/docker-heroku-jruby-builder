7f7c8e681574448fe712abe5be939b2da8e4ebee
Terence Lee <hone02@gmail.com>
2016-01-27 14:25:17 -0600

    JRuby 9.0.4.0

---
+++ rubies/cedar-14/ruby-2.2.2-jruby-9.0.4.0.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+source `dirname $0`/../common.sh
+source `dirname $0`/common.sh
+
+docker run -v $OUTPUT_DIR:/tmp/output -v $CACHE_DIR:/tmp/cache -e VERSION=9.0.4.0 -e RUBY_VERSION=2.2.2 -t hone/jruby-builder:$STACK
